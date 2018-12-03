ec99ca1cebf122496f0ad15f4cda46441ac7d006
Colton Morris <colton.morris@tcnbroadcasting.com>
2018-12-03 15:08:32 -0700

    more debug strings

--- generator/epic.go
+++ generator/epic.go
@@ -54,6 +54,9 @@ import * as protocActions from './actions_pb';
 import * as ProtocTypes from './protoc_types_pb';
 import * as ProtocServices from './protoc_services_pb';
 
+{{if .Debug}}console.log('ProtocTypes in state/epic: ', ProtocTypes);{{end}}
+{{if .Debug}}console.log('ProtocServices in state/epic: ', ProtocServices);{{end}}
+
 function createErrorObject(code: number|string|undefined, message: string): NodeJS.ErrnoException {
 	var err: NodeJS.ErrnoException = new Error();
 	err.message = message;
